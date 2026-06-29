6a175ded8decb0919a6462aa80f31425063048b4
Cristobal2929 <dayron100813@gmail.com>
2026-06-29 20:20:11 +0200

    Fénix intento 2: MainActivity

--- app/src/main/java/com/fenix/app/MainActivity.kt
+++ app/src/main/java/com/fenix/app/MainActivity.kt
@@ -17,6 +17,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONArray
 import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
